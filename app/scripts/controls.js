1f3e5ffdc0e6a22dfbd08788e20cdffd91b3166e
IronPeak <HrafnOrri1207@gmail.com>
2016-04-08 19:07:55 +0000

    Space is back

--- app/scripts/controls.js
+++ app/scripts/controls.js
@@ -10,9 +10,12 @@ window.Controls = (function() {
      */
     var Controls = function() {
         this.keys = {};
+		this.keydown = false;
 		this.mousedown = false;
         $(window).on('mousedown', this._onMouseDown.bind(this));
 		$(window).on('mouseup', this._onMouseUp.bind(this));
+		$(window).on('keydown', this._onKeyDown.bind(this));
+		$(window).on('keyup', this._onKeyUp.bind(this));
     };
 
     Controls.prototype._onMouseDown = function() {
@@ -22,13 +25,25 @@ window.Controls = (function() {
     Controls.prototype._onMouseUp = function() {
 		this.mousedown = false;
     };
+	
+	Controls.prototype._onKeyDown = function(e) {
+        this.keydown = (e.keyCode === 32);
+    };
+
+    Controls.prototype._onKeyUp = function(e) {
+		if(e.keyCode === 32)
+		{
+			this.keydown = false;
+		}
+    };
 
     /**
      * Only answers true once until a key is pressed again.
      */
     Controls.prototype.isJumping = function() {
-		var answer = this.mousedown;
+		var answer = this.mousedown || this.keydown;
 		this.mousedown = false;
+		this.keydown = false;
         return answer;
     };
     
